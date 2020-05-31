bdd87bb716f39cd5c10cbdc42281b2139b299243
Yates <2449711232@qq.com>
2020-05-31 23:40:24 +0800

    商品评论的完成

--- src/network/detail.js
+++ src/network/detail.js
@@ -9,6 +9,7 @@ export function getDetail(iid) {
   })
 }
 
+// 商品信息对象
 export class GoodsInfo {
   constructor(itemInfo, columns, services) {
     this.title = itemInfo.title;
@@ -18,10 +19,11 @@ export class GoodsInfo {
     this.discount = itemInfo.discountDesc;
     this.columns = columns;
     this.services = services;
-    this.realPrice = itemInfo.lowNowPrice
+    this.realPrice = itemInfo.lowNowPrice;
   }
 }
 
+// 商店信息对象
 export class Shop {
   constructor(shopInfo) {
     this.logo = shopInfo.shopLogo;
@@ -29,6 +31,16 @@ export class Shop {
     this.fans = shopInfo.cFans;
     this.sells = shopInfo.cSells;
     this.score = shopInfo.score;
-    this.goodsCount = shopInfo.cGoods
+    this.goodsCount = shopInfo.cGoods;
+  }
+}
+
+// 商品参数对象
+export class GoodsParam {
+  constructor(info, rule) {
+    // images可能没有值(某些商品有值，某些没有值)
+    this.image = info.images ? info.image[0] : '',
+    this.infos = info.set;
+    this.sizes = rule.tables;
   }
 }
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="detail">
     <detail-nav-bar class="detail-nav-bar"/>
-    <scroll class="content">
+    <scroll class="content" ref="scroll">
       <detail-swiper :topImages="topImages" class="detail-swiper"/>
       <detail-base-info :goodsInfo="goodsInfo"/>
       <detail-shop-info :shop="shop"/>
+      <detail-goods-info :detailInfo="detailInfo" @imagesLoad="imagesLoad"/>
+      <detail-param-info :paramInfo="paramInfo"/>
+      <detail-comment-info :commentInfo="commentInfo"/>
     </scroll>
   </div>
 </template>
 <script>
-import {getDetail, GoodsInfo, Shop} from 'network/detail'
+import {getDetail, GoodsInfo, Shop, GoodsParam} from 'network/detail'
 
 import DetailNavBar from './subCompoents/DetailNavBar'
 import DetailSwiper from './subCompoents/DetailSwiper'
 import DetailBaseInfo from './subCompoents/DetailBaseInfo'
 import DetailShopInfo from './subCompoents/DetailShopInfo'
+import DetailGoodsInfo from './subCompoents/DetailGoodsInfo'
+import DetailParamInfo from './subCompoents/DetailParamInfo'
+import DetailCommentInfo from './subCompoents/DetailCommentInfo'
 
 import Scroll from 'components/common/scroll/Scroll'
 export default {
@@ -24,7 +30,10 @@ export default {
       iid: null,
       topImages: [],
       goodsInfo: {},
-      shop: {}
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
+      commentInfo: {}
     }
   },
   components: {
@@ -32,6 +41,9 @@ export default {
     DetailSwiper,
     DetailBaseInfo,
     DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    DetailCommentInfo,
     Scroll
   },
   created() {
@@ -50,7 +62,23 @@ export default {
       
       // 创建店铺信息的对象
       this.shop = new Shop(data.shopInfo)
-    }) 
+
+      // 获取商品详情信息
+      this.detailInfo = data.detailInfo;
+
+      // 获取参数信息
+      this.paramInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
+
+      // 获取评论信息{}
+      if(data.rate.cRate !== 0) {
+        this.commentInfo = data.rate.list[0]
+      }
+    })  
+  },
+  methods: {
+    imagesLoad() {
+      this.$refs.scroll.refresh();
+    }
   }
 
 }
